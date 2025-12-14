ad9b7d11545a0b056dd5cd80dced73778dc165a7
vncapit <vncapit@gmail.com>
2025-12-14 22:23:57 +0700

    Post table add Slug

---
+++ backend/src/BlogApi/Migrations/20251214151032_PostTableAddSlug.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostTableAddSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Posts",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Posts");
+        }
+    }
+}
