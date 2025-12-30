4d17973faa92c7e1987c306ac5958bfff8c30316
vncapit <vncapit@gmail.com>
2025-12-30 21:59:53 +0700

    Admin panel: category - ok

--- frontend/BlogWeb/app/composables/api/admin/useCategoryApi.ts
+++ frontend/BlogWeb/app/composables/api/admin/useCategoryApi.ts
@@ -1,9 +1,37 @@
 import type { ApiResponse, Category } from '~/types';
 
-export const useCategoryApi = async () => {
+export const useCategoryApi = () => {
   const { $api } = useNuxtApp();
-  const res = await $api<ApiResponse<Category[]>>('Category/list', {
-    method: 'GET',
-  });
-  return res.data;
+  const list = async () => {
+    const res = await $api<ApiResponse<Category[]>>('Category/list', {
+      method: 'GET',
+    });
+    return res.data;
+  };
+
+  const add = async (category: Omit<Category, 'id'>) => {
+    const res = await $api<ApiResponse<Category[]>>('Category/add', {
+      method: 'POST',
+      body: category,
+    });
+    return res.data;
+  };
+
+  const update = async (category: Category) => {
+    const res = await $api<ApiResponse<Category[]>>('Category/update', {
+      method: 'PUT',
+      body: category,
+    });
+    return res.data;
+  };
+
+  const del = async (id: number) => {
+    const res = await $api<ApiResponse<Category[]>>('Category/delete', {
+      method: 'DELETE',
+      body: { id },
+    });
+    return res.data;
+  };
+
+  return { list, add, update, del };
 };
--- frontend/BlogWeb/app/pages/admin/category/index.vue
+++ frontend/BlogWeb/app/pages/admin/category/index.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="flex flex-col flex-1">
     <h1 class="text-2xl font-bold mb-4">Category Management</h1>
+    <div class="mt-2 mb-2">
+      <UButton @click="addCategory" color="primary"> Add Category </UButton>
+    </div>
     <UTable :data="categories || []" :columns="columns" class="flex-1" />
+
+    <UModal v-model:open="openForm" :dismissible="false" :title="formTitle">
+      <template #body>
+        <UForm ref="formRef" :schema="schema" :state="form" class="space-y-4">
+          <UFormField label="Category" name="name" required>
+            <UInput v-model="form.name" class="w-full" />
+          </UFormField>
+        </UForm>
+      </template>
+      <template #footer>
+        <div class="w-full flex justify-end gap-2">
+          <UButton variant="text" @click="openForm = false">Cancel</UButton>
+          <UButton @click="submitForm" form="category-form">Save</UButton>
+        </div>
+      </template>
+    </UModal>
   </div>
 </template>
 
@@ -9,24 +28,79 @@
   import { h, resolveComponent } from 'vue';
   import type { TableColumn } from '@nuxt/ui';
   import { useCategoryApi } from '~/composables/api/admin/useCategoryApi';
-  import { ref, onMounted } from 'vue';
+  import { ref, reactive, onMounted } from 'vue';
+  import * as z from 'zod';
+  import type { Category } from '~/types';
 
   definePageMeta({
     layout: 'admin',
   });
 
-  type Category = {
-    id: number;
-    name: string;
-  };
+  const toast = useToast();
+  const categoryApi = useCategoryApi();
 
   const UButton = resolveComponent('UButton');
+  const UPopover = resolveComponent('UPopover');
 
   const categories = ref<Category[]>([]);
 
-  onMounted(async () => {
-    categories.value = await useCategoryApi();
+  const formRef = useTemplateRef('formRef');
+  const schema = z.object({
+    name: z.string().min(1, 'Category name is required'),
   });
+
+  const defaultForm = { id: undefined as number | undefined, name: '' };
+  const openForm = ref(false);
+  const formTitle = ref('Add Category');
+
+  const form = reactive({ ...defaultForm });
+
+  onMounted(async () => {
+    categories.value = await categoryApi.list();
+  });
+
+  const submitForm = async () => {
+    try {
+      if (!(await formRef.value?.validate({}))) {
+        return;
+      }
+      if (form.id) {
+        categories.value = await categoryApi.update(form as Category);
+      } else {
+        categories.value = await categoryApi.add(form);
+      }
+      toast.add({
+        title: 'Success',
+        color: 'success',
+        duration: 1500,
+      });
+      openForm.value = false;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addCategory = () => {
+    formTitle.value = 'Add category';
+    Object.assign(form, defaultForm);
+    openForm.value = true;
+  };
+
+  const editCategory = (category: Category) => {
+    formTitle.value = 'Update category';
+    Object.assign(form, defaultForm, category);
+    openForm.value = true;
+  };
+  const deleteCategory = async (id: number) => {
+    try {
+      categories.value = await categoryApi.del(id);
+      toast.add({
+        title: 'Success',
+        color: 'success',
+        duration: 1500,
+      });
+    } catch (error) {}
+  };
 
   const columns: TableColumn<Category>[] = [
     {
@@ -63,28 +137,42 @@
           h(
             UButton,
             {
-              color: 'secondary',
-              onClick: () => editCategory(row.original.id),
+              color: 'warning',
+              onClick: () => editCategory(row.original),
             },
             'Edit'
           ),
-          h(
-            UButton,
-            {
-              color: 'error',
-              onClick: () => deleteCategory(row.original.id),
-            },
-            'Delete'
-          ),
+          h(UPopover, null, {
+            default: () =>
+              h(UButton, {
+                label: 'Delete',
+                color: 'error',
+              }),
+            content: ({ close }: { close: () => void }) =>
+              h('div', { class: 'p-3 space-y-2 w-48' }, [
+                h('p', 'Are you sure?'),
+                h('div', { class: 'flex justify-end gap-2' }, [
+                  h(UButton, {
+                    size: 'xs',
+                    variant: 'ghost',
+                    label: 'Cancel',
+                    onClick: close,
+                  }),
+
+                  h(UButton, {
+                    size: 'xs',
+                    variant: 'ghost',
+                    label: 'OK',
+                    onClick: () => {
+                      deleteCategory(row.original.id);
+                      close();
+                    },
+                  }),
+                ]),
+              ]),
+          }),
         ]);
       },
     },
   ];
-
-  const editCategory = (id: number) => {
-    console.log('Edit category with ID:', id);
-  };
-  const deleteCategory = (id: number) => {
-    console.log('Delete category with ID:', id);
-  };
 </script>
